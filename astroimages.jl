d2358a6141dd156b8a5cc9e72ef5c5f531dffc99
icweaver <iweaver@mailbox.org>
2022-05-17 15:39:30 -0400

    up

--- astroimages.jl
+++ astroimages.jl
@@ -9,8 +9,14 @@ begin
 	import Pkg
 	Pkg.activate(Base.current_project())
 	
-	using AstroImages, PlutoUI, Plots, ImageIO
+	using AstroImages, PlutoUI, Plots
 end
+
+# ╔═╡ 05adb41a-5481-478f-9f88-9f94df4fe4b4
+using UnitfulAstro, UnitfulAstro.Unitful
+
+# ╔═╡ 2d5365c9-c7d9-4d14-b074-f77b39baec02
+using AstroImages.AstroAngles
 
 # ╔═╡ 59584fac-598d-4752-82fa-840378d70c08
 Pkg.status()
@@ -19,10 +25,30 @@ Pkg.status()
 fname = download("http://data.astropy.org/tutorials/FITS-images/HorseHead.fits")
 
 # ╔═╡ a5d80eed-fb12-4980-82db-800a1c4dba49
-h = load(fname)
+img = load(fname)
+
+# ╔═╡ 0ce5fe45-dd19-4891-89a5-2c9f18daeed4
+l = @layout [
+	img{0.25w} colorbar{0.15h}
+]
+
+# ╔═╡ 5fcff0be-3d80-4423-a239-2a00aa376db3
+begin
+	implot(img; wcsticks=true, framestyle=:grid, layout=l)
+	# X, Y = world_to_pix(img, [hms"5 41 00"deg, dms"-2 28"deg])
+	# scatter!([X], [Y]; ms=10, msw=0)
+end
+
+# ╔═╡ 9dc84393-af39-4a3d-a878-ab4e924db480
+l
 
 # ╔═╡ Cell order:
 # ╠═f19b358c-8506-11ec-252c-c39dcd644d06
 # ╠═59584fac-598d-4752-82fa-840378d70c08
 # ╠═bb2425be-3e6a-456d-bad2-e665dc7408aa
 # ╠═a5d80eed-fb12-4980-82db-800a1c4dba49
+# ╠═5fcff0be-3d80-4423-a239-2a00aa376db3
+# ╠═9dc84393-af39-4a3d-a878-ab4e924db480
+# ╠═0ce5fe45-dd19-4891-89a5-2c9f18daeed4
+# ╠═05adb41a-5481-478f-9f88-9f94df4fe4b4
+# ╠═2d5365c9-c7d9-4d14-b074-f77b39baec02
